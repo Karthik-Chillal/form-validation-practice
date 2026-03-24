6d36fe268d32ab41f2dd92ed8bc4862dedd1cd0b
KarthikChillal <chillalkarthik@gmail.com>
2026-03-24 10:24:29 +0530

    final commit

--- src/submit.js
+++ src/submit.js
@@ -1,6 +1,7 @@
 import { fields } from './field-config.js';
 import { validate } from './validator.js';
 
+// Event Listener which fires up on clicking submit button and checks whether all input fields are valid or not
 document.getElementById('signup-form').addEventListener('submit', (e) => {
   e.preventDefault();
   let allValid = true;
